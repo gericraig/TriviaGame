fce0a1cd1ce69dbc6c92acc0ce41da0db7bb998f
Geralyn Craig <gericraig18@gmail.com>
2018-11-17 04:54:54 -0800

    Added more questions and made it pretty with added css code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,9 +8,108 @@ $(document).ready(function () {
             question: "Which Robin was murder by the Joker with a crow bar?",
             answers: ["Dick Grayson", "Jason Todd", "Time Drake", "Damian Wayne"],
             values: [false, true, false, false],
-            // details: write about the issue number
-            // gif: come up with a gif
-        }
+            detail: " After Jason is killed by the Joker and resurrected in the Lazarus Pit, he goes on to become the Red Hood.",
+            gif: "assets/images/1.gif",
+        },
+        {
+            question: "What super villain once broke Batman's back, leaving him crippled and wheelchair-bound?",
+            answers: ["Ra's Al Ghul", "Bane", "Joker", "Killer Croc"],
+            values: [false, true, false, false],
+            detail: "After deducing Batman's secret identity and invading the sanctity of Wayne Manor and the Batcave, Bane not only defeated Batman, but broke his back, leaving him crippled and wheelchair-bound.",
+            gif: "assets/images/2.gif",
+        },
+        {
+            question: "Which of the following characters did Bruce Wayne have a child with?",
+            answers: ["Poison Ivy", "Harley Quinn", "Talia Al Ghul", "Catwoman"],
+            values: [false, false, true, false],
+            detail: "Talia told Batman she'd had a miscarriage, but actually left the child (Damian Wayne) to be raised by the League of Assassins.",
+            gif: "assets/images/3.gif",
+        },
+        {
+            question: "What member of the Batman family became Oracle?",
+            answers: ["Huntress", "Catwoman", "Batwoman", "Batgirl"],
+            values: [false, false, false, true],
+            detail: "After being shot by the Joker and left paraplegic, Batgirl (Barbara Gordon) reinvents herself as the computer expert and information broker Oracle.",
+            gif: "assets/images/4.gif",
+        },
+        {
+            question: "Which of the following characters has NOT fought crime as Batgirl?",
+            answers: ["Laurel Lance", "Barbara Gordon", "Charlie Gage-Radcliffe", "Betty Kane"],
+            values: [true, false, false, false],
+            detail: "Betty Kane was the Golden Age Batgirl. Superpowered teen Charlie Gage-Radcliffe briefly donned the Batgirl costume after Barbara was injured. Laurel Lance is the Black Canary.",
+            gif: "assets/images/5.gif",
+        },
+        {
+            question: "Who first replaced Batman when he was paralyzed by Bane?",
+            answers: ["Azrael", "Robin", "Red Hood", "Nightwing"],
+            values: [true, false, false, false],
+            detail: "Azrael (Jean-Paul Valley) plays a pivotal role in the Knightfall story arc (1993-1994), in which he stands in as Batman after Bruce Wayne is defeated and paralyzed at the hands of Bane.",
+            gif: "assets/images/6.gif",
+        },
+        {
+            question: "What superpower does the Joker have?",
+            answers: ["super intelligence", "none", "poison breath", "super strength"],
+            values: [false, true, false, false],
+            detail: "He uses his expertise in chemical engineering to develop poisonous or lethal concoctions, and thematic weaponry, including razor-tipped playing cards, deadly joy buzzers, and acid-spraying lapel flowers.",
+            gif: "assets/images/7.gif",
+        },
+        {
+            question: "Where does Batman send his most twisted foes for rehabilitation?",
+            answers: ["Graystone Psychiatric Hospital", "Arkham Asylum", "Gotham Asylum", "Pennhurst Asylum"],
+            values: [false, true, false, false],
+            detail: "Located on the outskirts of Gotham, Arkham Asylum is a psychiatric hospital where many of Batman's foes are sent for rehabilitation. Unfortunately, inmates escape on a regular basis.",
+            gif: "assets/images/8.gif",
+        },
+        {
+            question: "What alias does Batman often use when working undercover?",
+            answers: ["Bruce Wayne", "Don Fortunato", "Matches Malone", "Carmine Falcone"],
+            values: [false, false, true, false],
+            detail: "Matches Malone is the name of a small-time criminal and a common guise adopted by Batman to infiltrate the Gotham's criminal underworld.",
+            gif: "assets/images/9.gif",
+        },
+        {
+            question: "When the Scarecrow used his fear gas on the Joker, what was the Clown Prince of Crime's greatest fear?",
+            answers: ["Someone else killing Batman", "Nothing", "Falling into a chemical vat", "Getting married to Harley Quinn"],
+            values: [false, true, false, false],
+            detail: "In Detective Comics #664, Scarecrow betrays Joker by spraying him with fear gas, but it has absolutely no effect; Joker then beats Scarecrow senseless with a chair.",
+            gif: "assets/images/10.gif",
+        },
+        {
+            question: "Who killed Batman's parents?",
+            answers: ["The Joker", "Carmine Falcone", "Sal Maroni", "Joe Chill"],
+            values: [false, false, false, true],
+            detail: "The mugger is not given a name until Batman #47 (June-July 1948) when Batman discovers that Joe Chill, the small-time crime boss he is investigating, is none other than the man who killed his parents.",
+            gif: "assets/images/11.gif",
+        },
+        {
+            questions: "Who killed Dick Grayson's parents?",
+            answers: ["Sal Maroni", "Tony Zucco", "Carmine Falcone", "Joe Chill"],
+            values: [false, true, false, false],
+            detail: "Zucco tries to extort the circus where the Flying Graysons are the main attraction. When the ringmaster refuses to pay, he sabotages the highwire ropes, which break and send Dick's parents falling to their deaths.",
+            gif: "assets/images/12.gif",
+        },
+        {
+            question: "Who is the Joker's accomplice and lover?",
+            answers: ["Harley Quinn", "Payton Riley", "Zatanna", "Lady Shiva"],
+            values: [true, false, false, false],
+            detail: "Harley Quinn met the Joker while working as a psychiatrist at Arkham Asylum. During therapy sessions, she fell in love with him and eventually helped him escape.",
+            gif: "assets/images/13.gif",
+        },
+        {
+            question: "What former District Attorney became the villain known as Two-Face?",
+            answers: ["Flod Lawton", "Jason Blood", "Harvey Dent", "Harvey Bullock"],
+            values: [false, false, true, false],
+            detail: "Harvey Dent went insane after a mob boss threw acidic chemicals at him during a trial, hideously scarring the left side of his face. He subsequently adopted the Two-Face persona, becoming a criminal obsessed with duality.",
+            gif: "assets/images/14.gif",
+        },
+        {
+            question: "Bruce Wayne's parents died outside of what theater?",
+            answers: ["Royal Theater", "Ridglea Theater", "Gotham Theater", "Monarch Theater"],
+            values: [false, false, false, true],
+            detail: "Thomas and Martha Wayne were walking home from the Monarch Theater with Bruce, when they were gunned down by a mugger who wanted Martha's pearl necklace.",
+            gif: "assets/images/15.webp",
+        },
+
     ]
 
     // Score
@@ -208,7 +307,7 @@ $(document).ready(function () {
         restart.addClass("restart")
         restart.text("Restart")
         restart.appendTo($("#content"))
-        
+
         $(".restart").on("click", function () {
             totalCorrect.remove();
             totalIncorrect.remove();
